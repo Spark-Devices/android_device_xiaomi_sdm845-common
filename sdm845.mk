a1d99ad086219e2af6539b08d3c4002950345b14
LuK1337 <priv.luk@gmail.com>
2021-04-14 14:29:58 +0200

    sdm845-common: Import QTI vibrator HAL
    Change-Id: I6ed3252c6da746c8fb24f44b3b3ec2b1f32a6cb2

--- sdm845.mk
+++ sdm845.mk
@@ -353,6 +353,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.xiaomi_sdm845
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
